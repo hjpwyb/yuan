ee577a9d735e76991b661ebad138cc798146d7c4
hjpwyb <122347230+hjpwyb@users.noreply.github.com>
2024-12-29 19:15:16 +0800

    Update update_domain.py

--- update_domain.py
+++ update_domain.py
@@ -19,7 +19,7 @@ def main():
     base_url = "http://7465ck.cc/vodtype/9-2.html"  # 要测试的基本 URL
 
     # 进行试错，依次更换URL中的数字部分
-    for i in range(7465, 7470):  # 假设你想测试7465ck.cc到7470ck.cc这几个域名
+    for i in range(7465, 7490):  # 假设你想测试7465ck.cc到7470ck.cc这几个域名
         url_to_test = base_url.replace("7465ck.cc", f"{i}ck.cc")
         
         # 检查URL有效性
